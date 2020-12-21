8a7c69f256260ef0304a40384281404cfe2cf646
Nikolaos Ilieskou <nikoloas.ilieskou@signify.com>
2020-12-21 09:41:45 +0100

    initial commit

---
+++ src/main/java/com/ilieskou/crossfitbackend/CrossfitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ilieskou.crossfitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossfitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossfitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilieskou/crossfitbackend/CrossfitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilieskou.crossfitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossfitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
